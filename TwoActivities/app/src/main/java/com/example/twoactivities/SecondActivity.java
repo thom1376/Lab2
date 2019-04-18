b7bcba0e5a84b0a303778e3b53de0327a88e5771
Logan Thomas <ltogan.2014@gmail.com>
2019-04-18 11:59:29 -0400

    Add EditText and Button to Second Activity layout

--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -15,5 +16,8 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+    }
+
+    public void returnReply(View view) {
     }
 }
