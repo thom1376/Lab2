2173ea4ad2b53d5957dafe41799117ae86e9a9e4
Logan Thomas <ltogan.2014@gmail.com>
2019-04-19 12:19:07 -0400

    Implement the openWebsite method

--- ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebsiteEditText = findViewById(R.id.website_edittext);
     }
 
     public void openWebsite(View view) {
+        // Get the URL text
+        String url = mWebsiteEditText.getText().toString();
+
+        // Parse the URI and create the Intent
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Find an activity to handle the intent and start it
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
     }
 
     public void openLocation(View view) {
