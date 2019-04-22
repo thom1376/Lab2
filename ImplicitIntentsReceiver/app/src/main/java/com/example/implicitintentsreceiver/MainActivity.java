0b7c169b3af5adf08a1ee5c67f8da428944ce13d
Logan Thomas <ltogan.2014@gmail.com>
2019-04-22 10:54:43 -0400

    Implement implicit intent reception in Implicit Intents Receiver

--- ImplicitIntentsReceiver/app/src/main/java/com/example/implicitintentsreceiver/MainActivity.java
+++ ImplicitIntentsReceiver/app/src/main/java/com/example/implicitintentsreceiver/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.implicitintentsreceiver;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+
+        if (uri != null) {
+            String uri_string = "URI: " + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uri_string);
+        }
     }
 }
